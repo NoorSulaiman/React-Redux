59e2028cd9e83e85a507936fa3f6ef4c7331159c
NoorSulaiman <noor.s.suliman@gmail.com>
2018-10-08 01:14:55 -0400

    created a lean version of react-redux module, Provider function and connect function

---
+++ src/react-redux.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const ctxStore = react.createContext();
+
+export const Provider = ({ store, children }) => {
+    return (
+        <ctxStore.Provider value={store}>
+            {children}
+        </ctxStore.Provider>
+    )
+};
+
+export const connect = (mapStateToProps, mapDispatchToProps) => {
+    return (WrappedComponent) => {
+        class ConnectedComponent extends React.Component {
+            unsubscribe = null;
+            componentDidMount() {
+                this.unsubscribe = this.props.subscribe(() => {
+                    this.setState({})
+                })
+            }
+            componentWillUnmount() {
+                this.unsubscribe()
+            }
+            render() {
+                const props = {
+                    ...this.props,
+                    ...mapStateToProps(this.props.getState()),
+                    ...mapDispatchToProps(this.props.dispatch)
+                }
+                return <WrappedComponent {...props} />
+            }
+        }
+
+        return () => (
+            <ctxStore.Consumer>
+                {store => <ConnectedComponent {...store} />}
+            </ctxStore.Consumer>
+        )
+    }
+};
